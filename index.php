8ac5e53148033ea858bc41d842c8623c91e7ca20
Desarrollo 2 <gustavojaimes1993@gmail.com>
2022-05-20 18:02:11 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./image/favicon3.png">
+    <title>TRES REYES CAJITITLAN</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/fuentes.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+    <main class="inicio">
+        <section class="banner-principal">
+            <div class="uk-text-center" uk-grid>
+                <div class="uk-width-1-1@m uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light video" uk-img>
+                    <iframe src="https://www.youtube-nocookie.com/embed/nhEX2hH5Cig?autoplay=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=1" width="1920" height="1080" allowfullscreen uk-responsive uk-video="automute: true; autoplay: true;"></iframe>
+                </div>
+                <div class="uk-width-1-1@m uk-flex uk-flex-center uk-flex-middle banner">
+                    <a href="./vistas/imagenes.php" class="uk-button uk-button-default uk-flex uk-flex-center" style="font-family: BellItalica;">CONOCER MÁS<div class="uk-flex uk-flex-center uk-flex-middle"><i class="fa fa-long-arrow-right flecha" style="padding-left: .2em;"></i></div></a>
+                </div>
+            </div>
+        </section>
+        <section class="banner-hojas">
+            <div class="uk-text-center" uk-grid>
+                <div class="uk-width-1-1@m">
+                    <div class="uk-text-center uk-background-contain uk-background-muted uk-height-large uk-panel uk-flex uk-flex-center uk-flex-middle banner" uk-grid>
+                        <div class="uk-width-1-4@m"></div>
+                        <div class="uk-width-expand@m">
+                            <p class="titulo">Tres Reyes es el equilibrio que estabas buscando</p>
+                            <p class="subtitulo">Alejate del bullicio de la ciudad, adquiere tu casa en una zona exclusiva, tranquila y segura.</p>
+                        </div>
+                        <div class="uk-width-1-4@m"></div>
+                    </div>
+                </div>
+                <div class="uk-width-1-1@m uk-margin-remove banner-torre">
+                    <img src="./image/imagenTorre.jpg"alt="">
+                </div>
+                <div class="uk-width-1-1@m uk-margin-remove letrero" style="">
+                    <div class="uk-text-center" uk-grid>
+                        <div class="uk-width-1-5@m"></div>
+                        <div class="uk-width-expand@m">
+                            <p class="titulo">Los mejores servicios para disfrutar los momentos valiosos de la vida</p>
+                            <p class="subtitulo">El complejo cuenta con una selección de servicios para brindarte una vida <br> activa en un entorno completamente seguro y rodeado de áreas verdes</p>
+                        </div>
+                        <div class="uk-width-1-5@m"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="iconos">
+            <div class="uk-text-center" uk-grid>
+                <div class="uk-width-1-6@m"></div>
+                <div class="uk-width-expand@m">
+                    <div class="uk-child-width-1-1 uk-child-width-1-3@s uk-grid-match" uk-grid>
+                        <div class="uk-animation-toggle" tabindex="0">
+                            <div class="uk-card uk-card-body uk-animation-shake uk-flex uk-flex-center uk-flex-middle container">
+                                <div class="sub-container">
+                                    <img src="./image/icono1.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-animation-toggle" tabindex="0">
+                            <div class="uk-card uk-card-body uk-animation-shake uk-flex uk-flex-center uk-flex-middle container">
+                                <div class="sub-container">
+                                    <img src="./image/icono2.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-animation-toggle" tabindex="0">
+                            <div class="uk-card uk-card-body uk-animation-shake uk-flex uk-flex-center uk-flex-middle container">
+                                <div class="sub-container">
+                                    <img src="./image/icono3.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-animation-toggle" tabindex="0">
+                            <div class="uk-card uk-card-body uk-animation-shake uk-flex uk-flex-center uk-flex-middle container">
+                                <div class="sub-container">
+                                    <img src="./image/icono4.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-animation-toggle" tabindex="0">
+                            <div class="uk-card uk-card-body uk-animation-shake uk-flex uk-flex-center uk-flex-middle container">
+                                <div class="sub-container">
+                                    <img src="./image/icono5.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-animation-toggle" tabindex="0">
+                            <div class="uk-card uk-card-body uk-animation-shake uk-flex uk-flex-center uk-flex-middle container">
+                                <div class="sub-container">
+                                    <img src="./image/icono6.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-width-1-6@m"></div>
+            </div>
+        </section>
+        <section class="carousel">
+            <div class="uk-text-center" uk-grid>
+                <div class="uk-width-auto@m"></div>
+                <div class="uk-width-expand@m container">
+                    <div uk-slider="center: false; autoplay: false; autoplay-interval: 2000;">
+                        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+
+                            <ul class="uk-slider-items uk-child-width-1-1 uk-child-width-1-2@m uk-grid">
+                                <li>
+                                    <div class="uk-panel video">
+                                        <iframe src="https://www.youtube-nocookie.com/embed/nhEX2hH5Cig?autoplay=0&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=1" width="1920" height="1080" allowfullscreen uk-responsive uk-video="automute: true;autoplay: false"></iframe>
+                                    </div>
+                                </li>
+                                <!-- <li> 
+                                    <div class="uk-panel video">
+                                        <iframe src="https://www.youtube-nocookie.com/embed/zgLIMUlATDE?autoplay=0&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=1" width="1920" height="1080" allowfullscreen uk-responsive uk-video="automute: true;autoplay: false"></iframe>
+                                    </div>
+                                </li> -->
+                                <li>
+                                    <div class="uk-panel video">
+                                        <iframe src="https://www.youtube-nocookie.com/embed/tfPNNcdDuM8?autoplay=0&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=1" width="1920" height="1080" allowfullscreen uk-responsive uk-video="automute: true;autoplay: false"></iframe>
+                                    </div>
+                                </li>
+                            </ul>
+
+                                <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                                <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+                        </div>
+                        <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+                    </div>
+                </div>
+                <div class="uk-width-auto@m"></div>
+            </div>
+        </section>
+    </main>
+    
+    <?php include './vistas/footer.php';?>
+
+    <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.13.7/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.13.7/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.13.7/dist/js/uikit-icons.min.js"></script>
+
+<!-- Recapchar -->
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+</body>
+</html>
